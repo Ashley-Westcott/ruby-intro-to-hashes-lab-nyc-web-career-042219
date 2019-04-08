0510a9337a3627fac9715d1bacda1fa7da27407b
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-08 05:10:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,9 +16,9 @@ def monopoly_with_second_tier
 
    monopoly = {
     :railroads => {
-      pieces: 4,
-      names: {},
-      rent_in_dollars: {}
+      :pieces => 4,
+      :names => {},
+      :rent_in_dollars => {}
     }
   }
 end
@@ -27,15 +27,15 @@ end
  def monopoly_with_third_tier	
 
  	monopoly = {
-    railroads: {
-      pieces: 4,
-      names: {
+    :railroads => {
+      :pieces => 4,
+     :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline => {}
       },
-      rent_in_dollars: {
+      :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
@@ -49,9 +49,9 @@ end
  def monopoly_with_fourth_tier
 
    monopoly = {
-    railroads: {
-      pieces: 4,
-      names: {
+    ::railroads => {
+      :pieces => 4,
+      :names => {
         :reading_railroad => {
           "mortgage_value" => "$100"
           },
@@ -65,7 +65,7 @@ end
             "mortgage_value" => "$800"
           }
       },
-      rent_in_dollars: {
+      :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
